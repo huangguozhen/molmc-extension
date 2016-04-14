4e7f21b775408caa46357d59b6fce922849c6dd9
huangguozhen <huangguozhen@outlook.com>
2016-04-14 11:10:13 +0800

    feat(gulp:chrome): add gulpfile chrome:images

---
+++ gulpfile.js/tasks/chrome-extension/images.js
@@ -0,0 +1,19 @@
+var changed     = require('gulp-changed')
+var gulp        = require('gulp')
+var imagemin    = require('gulp-imagemin')
+var path        = require('path')
+
+var paths = {
+  src: path.join('src/chrome-extension/images/**/*{' + ['png', 'jpg', 'gif'] + '}'),
+  dest: path.join('public/chrome-extension/images/')
+}
+
+var imagesTask = function() {
+  return gulp.src(paths.src)
+    .pipe(changed(paths.dest)) // Ignore unchanged files
+    .pipe(imagemin()) // Optimize
+    .pipe(gulp.dest(paths.dest))
+}
+
+gulp.task('chrome:images', imagesTask)
+module.exports = imagesTask
